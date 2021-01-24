4ba0d59c2f1cb4673459a93192a5154239eb3201
Anneke Schütte <a.schuette.hh@gmail.com>
2021-01-24 11:06:00 +0100

    Bar chart implementation

---
+++ components/energy-graph.stories.js
@@ -0,0 +1,9 @@
+import './enery-graph.stories.css'
+import html from './energy-graph.html'
+
+export default {
+  title: 'energy-graph',
+  component: html,
+}
+
+export const base = () => html
---
+++ components/energy-rate.stories.js
@@ -0,0 +1,9 @@
+import './enery-rate.stories.css'
+import html from './energy-rate.html'
+
+export default {
+  title: 'energy-rate',
+  component: html,
+}
+
+export const base = () => html
